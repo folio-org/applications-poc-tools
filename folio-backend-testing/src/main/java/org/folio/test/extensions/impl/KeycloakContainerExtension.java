1dad93ac6eb681ce73ffaf0826007ccdd6764715
Yauhen Vavilkin <yauhen_vavilkin@epam.com>
2024-03-05 15:47:42 +0400

    APPPOCTOOL-10: define keycloak admin client for KeycloakContainerExtension

--- folio-backend-testing/src/main/java/org/folio/test/extensions/impl/KeycloakContainerExtension.java
+++ folio-backend-testing/src/main/java/org/folio/test/extensions/impl/KeycloakContainerExtension.java
@@ -1,24 +1,34 @@
 package org.folio.test.extensions.impl;
 
+import static dasniko.testcontainers.keycloak.ExtendableKeycloakContainer.ADMIN_CLI_CLIENT;
+import static dasniko.testcontainers.keycloak.ExtendableKeycloakContainer.MASTER_REALM;
+import static jakarta.ws.rs.client.ClientBuilder.newBuilder;
+import static org.apache.http.conn.ssl.NoopHostnameVerifier.INSTANCE;
+import static org.apache.http.ssl.SSLContextBuilder.create;
 import static org.folio.test.TestUtils.parse;
 import static org.folio.test.TestUtils.readString;
 import static org.folio.test.TestUtils.readToFile;
+import static org.springframework.core.annotation.AnnotationUtils.findAnnotation;
+import static org.springframework.util.ResourceUtils.getFile;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import java.util.List;
+import javax.net.ssl.SSLContext;
 import lombok.extern.log4j.Log4j2;
+import org.apache.http.ssl.SSLInitializationException;
 import org.folio.test.extensions.EnableKeycloak;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.PartialImportRepresentation;
 
 @Log4j2
 public class KeycloakContainerExtension implements BeforeAllCallback, AfterAllCallback {
 
-  private static final String REALM = "master";
   private static final String REALM_JSON = "json/keycloak/master-realm.json";
   private static final String IMPORTED_CLIENT_ID = "folio-backend-admin-client";
   private static final String IMPORTED_CLIENT_SECRET = "supersecret";
@@ -30,7 +40,8 @@ public class KeycloakContainerExtension implements BeforeAllCallback, AfterAllCa
     if (!container.isRunning()) {
       container.start();
     }
-    ADMIN_CLIENT = container.getKeycloakAdminClient();
+
+    ADMIN_CLIENT = keycloakAdminClient(context, container);
 
     setupMasterRealm();
 
@@ -64,17 +75,36 @@ public class KeycloakContainerExtension implements BeforeAllCallback, AfterAllCa
     log.info("Setting up master realm");
     var realmJson = readString(REALM_JSON);
     var realmPartialImport = parse(realmJson, PartialImportRepresentation.class);
-    ADMIN_CLIENT.realm(REALM).partialImport(realmPartialImport);
+    ADMIN_CLIENT.realm(MASTER_REALM).partialImport(realmPartialImport);
   }
 
   @SuppressWarnings("resource")
   private static KeycloakContainer getKeycloakContainer(ExtensionContext context) {
-    var targetClass = context.getRequiredTestClass();
-    var annotation = targetClass.getAnnotation(EnableKeycloak.class);
-    var useTls = annotation.tlsEnabled();
-    return useTls
+    return isUseTls(context)
       ? keycloakContainer().useTlsKeystore("certificates/test.keystore.jks", "secretpassword")
       : keycloakContainer();
+  }
+
+  private static boolean isUseTls(ExtensionContext context) {
+    var targetClass = context.getRequiredTestClass();
+    var annotation = findAnnotation(targetClass, EnableKeycloak.class);
+    return annotation.tlsEnabled();
+  }
+
+  private static Keycloak keycloakAdminClient(ExtensionContext context, KeycloakContainer container) {
+    var builder = KeycloakBuilder.builder()
+      .realm(MASTER_REALM)
+      .serverUrl(container.getAuthServerUrl())
+      .clientId(ADMIN_CLI_CLIENT)
+      .username(container.getAdminUsername())
+      .password(container.getAdminPassword())
+      .grantType("password");
+
+    if (isUseTls(context)) {
+      builder.resteasyClient(buildResteasyClient());
+    }
+
+    return builder.build();
   }
 
   @SuppressWarnings("resource")
@@ -82,5 +112,20 @@ public class KeycloakContainerExtension implements BeforeAllCallback, AfterAllCa
     return new KeycloakContainer("quay.io/keycloak/keycloak:23.0.6")
       .withFeaturesEnabled("scripts", "token-exchange", "admin-fine-grained-authz")
       .withProviderLibsFrom(List.of(readToFile("keycloak/folio-scripts.jar", "folio-scripts", ".jar")));
+  }
+
+  private static ResteasyClient buildResteasyClient() {
+    return (ResteasyClient) newBuilder().sslContext(getSslContext()).hostnameVerifier(INSTANCE).build();
+  }
+
+  private static SSLContext getSslContext() {
+    try {
+      return create()
+        .loadTrustMaterial(getFile("classpath:certificates/test.truststore.jks"), "secretpassword".toCharArray())
+        .build();
+    } catch (Exception e) {
+      log.error("Error creating SSL context", e);
+      throw new SSLInitializationException("Error creating SSL context", e);
+    }
   }
 }
