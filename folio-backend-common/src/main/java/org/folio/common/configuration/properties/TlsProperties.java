6090ffa469dd37483f69a482ddef3183ed19f843
Taras_Spashchenko <Taras_Spashchenko@epam.com>
2024-05-14 13:46:13 +0200

    Add null check to getTrustStorePassword method
    A null checker has been added in the `getTrustStorePassword` method in the `TlsProperties` class. Now, if the `trustStorePassword` is null, an empty string will be returned instead, preventing potential null pointer exceptions.

--- folio-backend-common/src/main/java/org/folio/common/configuration/properties/TlsProperties.java
+++ folio-backend-common/src/main/java/org/folio/common/configuration/properties/TlsProperties.java
@@ -1,5 +1,7 @@
 package org.folio.common.configuration.properties;
 
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,4 +17,8 @@ public class TlsProperties {
   private String trustStorePath;
   private String trustStorePassword;
   private String trustStoreType;
+
+  public String getTrustStorePassword() {
+    return trustStorePassword == null ? EMPTY : trustStorePassword;
+  }
 }
