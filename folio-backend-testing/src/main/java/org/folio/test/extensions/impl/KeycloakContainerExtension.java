56d5f65c7cc0bba5ce2b399efcb54f4c4da47aca
Pavel Filippov <pavel_filippov@epam.com>
2025-02-03 15:11:28 +0200

    KEYCLOAK-37 Add support for Keycloak v26.1.0

--- folio-backend-testing/src/main/java/org/folio/test/extensions/impl/KeycloakContainerExtension.java
+++ folio-backend-testing/src/main/java/org/folio/test/extensions/impl/KeycloakContainerExtension.java
@@ -28,7 +28,7 @@ import org.keycloak.representations.idm.PartialImportRepresentation;
 @Log4j2
 public class KeycloakContainerExtension implements BeforeAllCallback, AfterAllCallback {
 
-  private static final String KEYCLOAK_IMAGE = "quay.io/keycloak/keycloak:25.0.1";
+  private static final String KEYCLOAK_IMAGE = "quay.io/keycloak/keycloak:26.1.0";
   private static final String REALM_JSON = "json/keycloak/master-realm.json";
   private static final String IMPORTED_CLIENT_ID = "folio-backend-admin-client";
   private static final String IMPORTED_CLIENT_SECRET = "supersecret";
