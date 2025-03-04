6936cd486335d9080c8e648d2882e5352a80e2ca
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2025-03-04 14:51:52 +0200

    fix import order

--- folio-integration-kong/src/test/java/org/folio/tools/kong/service/KongGatewayServiceTest.java
+++ folio-integration-kong/src/test/java/org/folio/tools/kong/service/KongGatewayServiceTest.java
@@ -1,47 +1,23 @@
 package org.folio.tools.kong.service;
 
-import feign.FeignException.InternalServerError;
-import feign.FeignException.NotFound;
 import static feign.Request.HttpMethod.GET;
 import static feign.Request.HttpMethod.PUT;
 import static feign.Request.create;
-import feign.RequestTemplate;
 import static java.lang.String.format;
 import static java.lang.String.join;
-import java.util.ArrayList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonList;
 import static java.util.Collections.singletonMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
 import static java.util.stream.Collectors.joining;
-import java.util.stream.Stream;
 import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.config.Configurator;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.folio.common.domain.model.InterfaceDescriptor;
 import static org.folio.common.domain.model.InterfaceDescriptor.SYSTEM_INTERFACE_TYPE;
 import static org.folio.common.domain.model.InterfaceDescriptor.TIMER_INTERFACE;
-import org.folio.common.domain.model.ModuleDescriptor;
-import org.folio.common.domain.model.RoutingEntry;
-import org.folio.common.domain.model.error.Parameter;
 import static org.folio.common.utils.OkapiHeaders.MODULE_ID;
 import static org.folio.common.utils.OkapiHeaders.TENANT;
 import static org.folio.common.utils.UuidUtils.randomId;
-import org.folio.test.types.UnitTest;
-import org.folio.tools.kong.client.KongAdminClient;
-import org.folio.tools.kong.client.KongAdminClient.KongResultList;
-import org.folio.tools.kong.exception.KongIntegrationException;
-import org.folio.tools.kong.model.Identifier;
-import org.folio.tools.kong.model.Route;
-import org.folio.tools.kong.model.Service;
 import static org.folio.tools.kong.service.KongGatewayServiceTest.TestValues.kongService;
 import static org.folio.tools.kong.service.KongGatewayServiceTest.TestValues.mdWithMultipleInterface1;
 import static org.folio.tools.kong.service.KongGatewayServiceTest.TestValues.mdWithMultipleInterface2;
@@ -49,6 +25,40 @@ import static org.folio.tools.kong.service.KongGatewayServiceTest.TestValues.mdW
 import static org.folio.tools.kong.service.KongGatewayServiceTest.TestValues.moduleDescriptor;
 import static org.folio.tools.kong.service.KongGatewayServiceTest.TestValues.multipleTypeHeaders;
 import static org.folio.tools.kong.service.KongGatewayServiceTest.TestValues.route;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import feign.FeignException.InternalServerError;
+import feign.FeignException.NotFound;
+import feign.RequestTemplate;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.folio.common.domain.model.InterfaceDescriptor;
+import org.folio.common.domain.model.ModuleDescriptor;
+import org.folio.common.domain.model.RoutingEntry;
+import org.folio.common.domain.model.error.Parameter;
+import org.folio.test.types.UnitTest;
+import org.folio.tools.kong.client.KongAdminClient;
+import org.folio.tools.kong.client.KongAdminClient.KongResultList;
+import org.folio.tools.kong.exception.KongIntegrationException;
+import org.folio.tools.kong.model.Identifier;
+import org.folio.tools.kong.model.Route;
+import org.folio.tools.kong.model.Service;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -57,18 +67,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.util.StringUtils;
 import org.mockito.ArgumentCaptor;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @UnitTest
@@ -445,7 +446,7 @@ class KongGatewayServiceTest {
       var cause = new RuntimeException("Test");
       when(kongAdminClient.getServiceRoutes(MOD_ID, null)).thenThrow(cause);
       assertThatThrownBy(() -> kongGatewayService.deleteServiceRoutes(MOD_ID)).isInstanceOf(
-        KongIntegrationException.class).hasCause(cause)
+          KongIntegrationException.class).hasCause(cause)
         .hasMessage("Failed to delete all routes for service " + MOD_ID);
     }
   }
