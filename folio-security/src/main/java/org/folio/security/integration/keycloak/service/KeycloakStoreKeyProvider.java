4be32751653b4319d593a06a65a4f633f7ca2fbb
Julian Ladisch <julianladisch@users.noreply.github.com>
2025-08-14 11:05:40 +0200

    APPPOCTOOL-63: https://folio-org.atlassian.net/browse/APPPOCTOOL-63
    Summary:
    
    Use the SECURE_STORE_ENV environment variable, not the ENV environment variable, as the first component of the secure store key.
    
    Description:
    
    It’s a well known pattern to run a single Kafka cluster for multiple flower releases: https://github.com/folio-org/rfcs/blob/master/text/0002-kafka-tenant-collection-topics.md#migration-to-new-module-version
    
    The ENV environment variable is used as the first component of the Kafka topic. Setting ENV to the flower name (like ramsons or sunflower) ensures that topics are produced and consumed by modules of the same flower release only.
    
    Using ENV for the secure store key is wrong because a password doesn’t change when migrating a tenant from ramsons to sunflower.
    
    There can be a need to separate the entries in the secure store, for example using prod and test, but not by using the flower names from the ENV environment variable.
    
    applications-poc-tools supports modules that have application.yaml with this entry: `folio.environment: ${ENV:folio}`
    
    We need to add support for this additional entry in application.yaml: `application.secure-store.environment: ${SECURE_STORE_ENV:${ENV:folio}}`
    
    KeycloakSecretUtils makes unit tests difficult because it is a static utility class that directly accesses environment variables without caching.
    
    Using KeycloakStoreKeyProvider and SecureStoreProperties instead that allow for dependency injection and configuration caching solves that problem.
    
    Fix, see https://folio-org.atlassian.net/browse/FOLIO-4344 :
    
    * replace ENV by SECURE_STORE_ENV environment variable if used for secure store key
    * fall back to ENV if SECURE_STORE_ENV is not defined to ensure upgrade compatibility for Ramsons CSP and Sunflower CSP; this fall back is not used for Trillium
    * fall back to folio if neither SECURE_STORE_ENV nor ENV are defined (the same as the ENV default to ensure compatibility)
    * Drop KeycloakSecretUtils, provide KeycloakStoreKeyProvider and SecureStoreProperties instead.

---
+++ folio-security/src/main/java/org/folio/security/integration/keycloak/service/KeycloakStoreKeyProvider.java
@@ -0,0 +1,29 @@
+package org.folio.security.integration.keycloak.service;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+import lombok.RequiredArgsConstructor;
+import org.folio.tools.store.properties.SecureStoreProperties;
+
+@RequiredArgsConstructor
+public class KeycloakStoreKeyProvider {
+
+  public static final String GLOBAL_SECTION = "master";
+
+  private final SecureStoreProperties secureStoreProperties;
+
+  public String globalStoreKey(String clientId) {
+    return tenantStoreKey(GLOBAL_SECTION, clientId);
+  }
+
+  public String tenantStoreKey(String tenant, String clientId) {
+    if (isBlank(tenant)) {
+      throw new IllegalArgumentException("Tenant cannot be empty");
+    }
+    if (isBlank(clientId)) {
+      throw new IllegalArgumentException("Client id cannot be empty");
+    }
+
+    return String.format("%s_%s_%s", secureStoreProperties.getSecureStoreEnvironment(), tenant, clientId);
+  }
+}
