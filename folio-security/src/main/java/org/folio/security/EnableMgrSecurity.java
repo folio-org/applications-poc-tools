03938e9ceb6b4be7411193264bb9293116955def
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2025-11-24 15:17:23 +0300

    APPOCTOOL-71 - exclude UserDetailsServiceAutoConfiguration

--- folio-security/src/main/java/org/folio/security/EnableMgrSecurity.java
+++ folio-security/src/main/java/org/folio/security/EnableMgrSecurity.java
@@ -5,9 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.folio.security.configuration.SecurityConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Import({SecurityConfiguration.class})
+@EnableAutoConfiguration(exclude = {UserDetailsServiceAutoConfiguration.class})
 public @interface EnableMgrSecurity {}
