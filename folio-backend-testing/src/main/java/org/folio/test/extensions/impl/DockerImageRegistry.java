95580541d402d8dbfee1a0f3400d2efd9b3828c8
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2026-03-25 13:24:18 +0200

    APPPOCTOOL-78 minor fixes (#307)

--- folio-backend-testing/src/main/java/org/folio/test/extensions/impl/DockerImageRegistry.java
+++ folio-backend-testing/src/main/java/org/folio/test/extensions/impl/DockerImageRegistry.java
@@ -5,15 +5,15 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class DockerImageRegistry {
 
-  public static String POSTGRES_IMAGE_ENV = "TESTCONTAINERS_POSTGRES_IMAGE";
-  public static String WIREMOCK_IMAGE_ENV = "TESTCONTAINERS_WIREMOCK_IMAGE";
-  public static String KEYCLOAK_IMAGE_ENV = "TESTCONTAINERS_KEYCLOAK_IMAGE";
-  public static String KAFKA_IMAGE_ENV    = "TESTCONTAINERS_KAFKA_IMAGE";
+  public static final String POSTGRES_IMAGE_ENV = "TESTCONTAINERS_POSTGRES_IMAGE";
+  public static final String WIREMOCK_IMAGE_ENV = "TESTCONTAINERS_WIREMOCK_IMAGE";
+  public static final String KEYCLOAK_IMAGE_ENV = "TESTCONTAINERS_KEYCLOAK_IMAGE";
+  public static final String KAFKA_IMAGE_ENV    = "TESTCONTAINERS_KAFKA_IMAGE";
 
-  public static String POSTGRES_DEFAULT_IMAGE = "postgres:16-alpine";
-  public static String WIREMOCK_DEFAULT_IMAGE = "wiremock/3.13.2-2-alpine";
-  public static String KEYCLOAK_DEFAULT_IMAGE = "quay.io/keycloak/keycloak:26.5.2";
-  public static String KAFKA_DEFAULT_IMAGE    = "apache/kafka-native:3.8.0";
+  public static final String POSTGRES_DEFAULT_IMAGE = "postgres:16-alpine";
+  public static final String WIREMOCK_DEFAULT_IMAGE = "wiremock/3.13.2-2-alpine";
+  public static final String KEYCLOAK_DEFAULT_IMAGE = "quay.io/keycloak/keycloak:26.5.2";
+  public static final String KAFKA_DEFAULT_IMAGE    = "apache/kafka-native:3.8.0";
 
   public static String getPostgresImageName() {
     return getImageName(POSTGRES_IMAGE_ENV, POSTGRES_DEFAULT_IMAGE);
