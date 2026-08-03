6332b191ddf9bbaa6de1fb5df1bc744c0ffc9515
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2026-08-03 18:06:59 +0300

    feat(model): add ResourceResultEvent and ResourceResultStatus classes

---
+++ folio-integration-kafka/folio-kafka-common/src/main/java/org/folio/integration/kafka/model/ResourceResultEvent.java
@@ -0,0 +1,32 @@
+package org.folio.integration.kafka.model;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_NULL)
+public class ResourceResultEvent implements TenantAwareEvent {
+
+  @NotBlank
+  private String id;
+  @NotBlank
+  private String tenant;
+
+  @Nullable
+  private String moduleId;
+  private String resourceName;
+
+  private ResourceResultStatus status;
+  @Nullable
+  private String details;
+}
