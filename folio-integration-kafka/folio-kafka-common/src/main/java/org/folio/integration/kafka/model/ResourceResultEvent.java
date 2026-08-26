3fc7b2506d809c55044756c19961a1141bb1110c
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2026-08-26 14:38:58 +0300

    MGRENTITLE-158 Add not-null constraint to resource result status (#361)

--- folio-integration-kafka/folio-kafka-common/src/main/java/org/folio/integration/kafka/model/ResourceResultEvent.java
+++ folio-integration-kafka/folio-kafka-common/src/main/java/org/folio/integration/kafka/model/ResourceResultEvent.java
@@ -4,6 +4,7 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,6 +27,7 @@ public class ResourceResultEvent implements TenantAwareEvent {
   private String moduleId;
   private String resourceName;
 
+  @NotNull
   private ResourceResultStatus status;
   @Nullable
   private String details;
